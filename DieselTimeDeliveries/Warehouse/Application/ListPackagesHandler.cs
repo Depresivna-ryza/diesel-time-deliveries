843bec0f9c38e2f719d1acf768eef85c17f24fa2
Richard Schwarz <521781@mail.muni.cz>
2025-02-01 02:49:32 +0100

    feat(warehouse/package) add package list endpoint

---
+++ DieselTimeDeliveries/Warehouse/Application/ListPackagesHandler.cs
@@ -0,0 +1,29 @@
+﻿using Warehouse.Domain.Models.Package;
+using Warehouse.Domain.Services;
+using ErrorOr;
+
+namespace Warehouse.Application;
+
+public record ListPackagesQuery(int Page, int PageSize)
+{ 
+    public record Result(IEnumerable<Package> Packages);
+    public record Package(Guid PackageId, string Name, decimal Weight, string Destination, string Status);
+}
+
+public class ListPackagesHandler(IQueryObject<Package> queryObject)
+{
+    public async Task<ErrorOr<ListPackagesQuery.Result>> Handle(ListPackagesQuery query)
+    {
+        var packages = await queryObject.Page(query.Page, query.PageSize).ExecuteAsync();
+        
+        return new ListPackagesQuery.Result(
+            packages.Select(p => new ListPackagesQuery.Package(
+                p.Id.Value,
+                p.Name,
+                p.Weight.Value,
+                p.Destination,
+                p.Status.ToString()
+            ))
+        );
+    }
+}
