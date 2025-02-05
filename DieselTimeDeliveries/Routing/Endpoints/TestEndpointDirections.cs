37501806069c8aa6232e1ab06dce19534e5fc7ca
Mário Hatalčík <xhatalc@fi.muni.cz>
2025-02-05 18:35:09 +0100

    add contracts for routing project

---
+++ DieselTimeDeliveries/Routing/Endpoints/TestEndpointDirections.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Routing.Application;
+using Wolverine;
+using ErrorOr;
+using Routing.Contracts.Commands;
+using Wolverine.Http;
+
+namespace Routing.Endpoints;
+
+public record DirectionsRequest(string origin, string destination)
+{
+    public record Response(List<string> Direction) { }
+}
+
+public class TestEndpointDirections //only for testing purposes
+{
+    [Tags("Routing - Package")]
+    [WolverinePost("/testdirection")]
+    public static async Task<IResult> RoutePackagesAsync(DirectionsRequest destinatinRouteRequest, IMessageBus sender)
+    {
+        var command = new GetDirectionsCommand(destinatinRouteRequest.origin, destinatinRouteRequest.destination);
+
+        var result = await sender.InvokeAsync<ErrorOr<GetDirectionsCommand.Result>>(command);
+
+        return result.Match(
+            value => Results.Ok(value.StepInstructions),
+            errors => Results.BadRequest(errors.Select(e => e.Code))
+        );
+    }
+}
+
