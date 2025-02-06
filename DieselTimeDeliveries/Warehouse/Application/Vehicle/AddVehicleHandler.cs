c99c3965b31904f7bf93ecd4645c8218d85fb9d8
Richard Schwarz <521781@mail.muni.cz>
2025-02-06 03:21:59 +0100

    feat(warehouse/vehicle): add vehicle endpoints+handlers (add, get, list all)

---
+++ DieselTimeDeliveries/Warehouse/Application/Vehicle/AddVehicleHandler.cs
@@ -0,0 +1,35 @@
+﻿using Warehouse.Domain.Models.Vehicle;
+using Warehouse.Domain.Services;
+
+namespace Warehouse.Application;
+using ErrorOr;
+
+public record AddVehicleCommand(string Make, string Model, decimal WeightLimit, string Vin)
+{
+    public record Result(Guid VehicleId, string Make, string Model, decimal WeightLimit, string Vin, string Status);
+
+}
+
+public class AddVehicleHandler(IRepository<Vehicle> repository)
+{
+    public async Task<ErrorOr<AddVehicleCommand.Result>> HandleAsync(AddVehicleCommand command)
+    {
+        var goods = Vehicle.Create(command.Make, command.Model, command.WeightLimit, command.Vin);
+
+        if (goods.IsError)
+            return goods.Errors;
+        
+        var addedGoods = await repository.InsertAsync(goods.Value);
+        // addedGoods.VehicleAdded();
+        
+        await repository.CommitAsync();
+        return new AddVehicleCommand.Result(
+            addedGoods.Id.Value,
+            addedGoods.Make,
+            addedGoods.Model,
+            addedGoods.PackageWeightLimit.Value,
+            addedGoods.Vin.Value,
+            addedGoods.Status.ToString()
+        );
+    }
+}
---
+++ DieselTimeDeliveries/Warehouse/Application/Vehicle/GetVehicleHandler.cs
@@ -0,0 +1,32 @@
+﻿using Marten;
+using Warehouse.Domain.Models.Vehicle;
+using Warehouse.Domain.Services;
+
+namespace Warehouse.Application;
+using ErrorOr;
+
+public record GetVehicleQuery(Guid VehicleId)
+{ 
+    public record Result(Guid VehicleId, string Make, string Model, decimal WeightLimit, string Vin, string Status);
+}
+
+
+public class GetVehicleHandler(IQueryObject<Vehicle> queryObject)
+{
+    public async Task<ErrorOr<GetVehicleQuery.Result>> Handle(GetVehicleQuery query)
+    {
+        var vehicle = (await queryObject.Filter(p => p.Id == VehicleId.Create(query.VehicleId)).ExecuteAsync()).SingleOrDefault();
+
+        if (vehicle is null)
+            return Error.Validation($"Vehicle (id: {query.VehicleId}) not found");
+
+        return new GetVehicleQuery.Result(
+            vehicle.Id.Value,
+            vehicle.Make,
+            vehicle.Model,
+            vehicle.PackageWeightLimit.Value,
+            vehicle.Vin.Value,
+            vehicle.Status.ToString()
+        );
+    }
+}
