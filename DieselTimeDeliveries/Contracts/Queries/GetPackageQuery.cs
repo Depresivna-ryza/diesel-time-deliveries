b37c032b69dce08bde980b607ba00aca3b3d226b
Richard Schwarz <521781@mail.muni.cz>
2025-02-07 16:29:06 +0100

    feat(warehouse): add get package contract and handler

---
+++ DieselTimeDeliveries/Contracts/Queries/GetPackageQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Contracts.Queries;
+
+public record GetPackageQuery(Guid PackageId)
+{
+    public record Result(decimal weight, string destination, string status);
+}
---
+++ DieselTimeDeliveries/Warehouse/Application/CommandHandlers/GetPackageHandler.cs
@@ -0,0 +1,24 @@
+﻿using Contracts.Queries;
+using Warehouse.Domain.Services;
+using ErrorOr;
+using Warehouse.Domain.Models.Courier;
+using Warehouse.Domain.Models.Package;
+
+namespace Warehouse.Application.CommandHandlers;
+
+public class GetPackageHandler(
+    IQueryObject<Domain.Models.Package.Package> queryObject)
+{
+    public async Task<ErrorOr<GetPackageQuery.Result>> HandleAsync(GetPackageQuery query)
+    {
+        var package = (await queryObject.Filter(p => p.Id == PackageId.Create(query.PackageId)).ExecuteAsync())
+            .SingleOrDefault();
+        
+        if (package is null)
+        {
+            return Error.Validation("No available couriers");
+        }
+        
+        return new GetPackageQuery.Result(package.Weight.Value, package.Destination, package.Status.ToString());
+    }
+}
