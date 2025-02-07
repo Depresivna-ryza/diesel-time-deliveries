02835d39582b8f354a07de842dd6b7ab6ceaea29
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-02-07 15:06:31 +0100

    Feat: Add Plan Configuration

--- DieselTimeDeliveries/Delivery/Infrastructure/Configurations/PlanConfiguration.cs
+++ DieselTimeDeliveries/Delivery/Infrastructure/Configurations/PlanConfiguration.cs
@@ -1,6 +1,21 @@
-﻿namespace Delivery.Infrastructure.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Delivery.Domain.Models;
 
-public class PlanConfiguration
+namespace Delivery.Infrastructure.Configurations;
+
+public class PlanConfiguration : IEntityTypeConfiguration<Plan>
 {
-    
+    public void Configure(EntityTypeBuilder<Plan> builder)
+    {
+        builder.HasKey(g => g.Id);
+
+        builder.Property(g => g.Id)
+            .HasColumnName(nameof(PlanId))
+            .ValueGeneratedNever()
+            .HasConversion(
+                planId => planId.Value,
+                plan => PlanId.Create(plan)
+            );
+    }
 }
